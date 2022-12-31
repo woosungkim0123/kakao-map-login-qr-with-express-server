7c0cdae439fc1ad3b0b7bdfa0fbbc05c062458ed
Woosung_Kim <sibu2005@naver.com>
2022-12-31 19:49:13 +0900

    change port

--- src/server/bin/www.js
+++ src/server/bin/www.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import "regenerator-runtime";
 import app from "../app";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
   console.info(`서버가 열렸습니다. http://localhost:${PORT}`)
