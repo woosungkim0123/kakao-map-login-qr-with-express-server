8c23733a75ae4a3017d27d07144b5d6514729964
Woosung_Kim <sibu2005@naver.com>
2023-02-12 23:04:18 +0900

    cors error fix

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-
+import cors from "cors";
 import mainRouter from "./router/mainRouter";
 import userRouter from "./router/userRouter";
 
@@ -9,9 +9,15 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "src/client/views");
 
+app.use(cors({
+  origin: "https://master--candid-halva-4d19f5.netlify.app",
+  method: "GET, POST, PUT, DELETE, PATCH",
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/client", express.static("src/client"));
 app.use("/file", express.static("file"));
