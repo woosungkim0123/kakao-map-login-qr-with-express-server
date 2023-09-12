01e692840c0543c78253535bdee369a108f83278
Woosung_Kim <sibu2005@naver.com>
2023-09-13 08:53:37 +0900

    주석 추가 및 리팩토링 진행

--- src/app.js
+++ src/app.js
@@ -5,12 +5,31 @@ import courseRouter from "./router/courseRouter";
 
 const app = express();
 
+/**
+ * CORS (Cross-Origin Resource Sharing)는 웹 페이지의 요청 도메인과 응답 도메인이 다를 때
+ * 안전하게 요청을 처리할 수 있도록 하는 웹 브라우저의 보안 기술입니다.
+ * 프론트 서버를 따로 사용시 (예: React, Vue 등) ALLOWED_ORIGINS 배열에 추가해야 합니다.
+ */
+const ALLOWED_ORIGINS = [
+  "https://master--candid-halva-4d19f5.netlify.app", 
+  "http://localhost:3000"
+];
 app.use(cors({
-  origin: ["https://master--candid-halva-4d19f5.netlify.app", "http://localhost:3000"],
+  origin: ALLOWED_ORIGINS,
   method: "GET, POST, PUT, DELETE, PATCH",
   credentials: true,
 }));
+
+/**
+ * express.json() 미들웨어를 사용하여 들어오는 요청의 JSON 본문을 파싱합니다.
+ * 이를 통해 req.body로 JSON 데이터에 접근할 수 있습니다.
+ */
 app.use(express.json());
+
+/**
+ * express.urlencoded() 미들웨어는 URL-encoded 데이터를 파싱합니다.
+ * 이 미들웨어는 폼 데이터(form data)를 처리할 때 주로 사용됩니다.
+ */
 app.use(express.urlencoded({ extended: true }));
 
 
--- src/bin/www.js
+++ src/bin/www.js
@@ -5,6 +5,9 @@ import app from "../app";
 
 const PORT = process.env.PORT || 3001;
 
+/**
+ * 해당 포트로 백엔드 서버 오픈
+ */
 app.listen(PORT, () =>
   console.info(`Server Open :::::: http://localhost:${PORT}`)
 );
