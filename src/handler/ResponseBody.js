1a513009ced47ed4f29a8b05209abc099a7e2096
Woosung_Kim <sibu2005@naver.com>
2023-09-15 11:42:22 +0900

    대소문자로인한 삭제

--- src/handler/ResponseBody.js
+++
@@ -1,8 +0,0 @@
-export default class responseBody {
-  constructor (statusCode, statusText, message, data) {
-    this.statusCode = statusCode;
-    this.statusText = statusText;
-    this.message = message;
-    this.data = data;
-  }
-}
--- src/middleware/validation.js
+++
@@ -1,8 +0,0 @@
-import exception from "../handler/exception.js";
-
-export const qrForm = (req, res, next) => {
-  const { qrCode, latitude, longitude } = req.body;
-  if(!qrCode) return res.status(exception.QR_EMPTY.statusCode).json(exception.QR_EMPTY);
-  else if(!latitude || !longitude) return res.status(exception.LOCATION_EMPTY.statusCode).json(exception.LOCATION_EMPTY);
-  next();
-}
